f9352f868c1769c3ebd6c0f5e9f48e54a264fb97
Daniel Di Leva <danieldileva@hotmail.com>
2022-02-08 19:07:31 -0300

    Agregue menu

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+return(
+    <nav class="navbar">
+
+        <a>inicio</a>
+        <a>productos</a>c   
+    </nav>
+)
+}
+
+export default NavBar;
